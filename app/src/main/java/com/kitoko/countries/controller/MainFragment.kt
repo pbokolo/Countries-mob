657b4639601c8aba8f681182c9b71ee472b686ac
pbokolo <dev.pbokolo@gmail.com>
2023-12-03 21:18:54 +0200

    Using viewBinding in the MainFragment

--- app/src/main/java/com/kitoko/countries/controller/MainFragment.kt
+++ app/src/main/java/com/kitoko/countries/controller/MainFragment.kt
@@ -5,18 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kitoko.countries.R
+import com.kitoko.countries.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-
+    private var vBinder: FragmentMainBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val binding = FragmentMainBinding.inflate(inflater, container, false)
+        vBinder = binding
+        return binding.root
     }
 
 
