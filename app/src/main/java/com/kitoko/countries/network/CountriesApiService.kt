5223ff03e78ad5aa6c7a0dc7f3543eb86943fc15
pbokolo <dev.pbokolo@gmail.com>
2023-12-03 14:52:45 +0200

    Finnishing up the setup of the network layer by definning a higher level interface and a public object in order to properly use retrofit in the app

--- app/src/main/java/com/kitoko/countries/network/CountriesApiService.kt
+++ app/src/main/java/com/kitoko/countries/network/CountriesApiService.kt
@@ -1,2 +1,30 @@
 package com.kitoko.countries.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://restcountries.com/v3.1"
+
+// Builds and create a retrofit object
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL).build()
+
+/**
+ * The following interface defines how retrofit will be talking to the server using http
+ */
+interface CountriesApiService{
+    @GET("all")
+    fun getAll():String
+}
+
+/**
+ * The following defines a public and unique retrofit object that will be used oll over the app
+ */
+object CountriesApi{
+
+    val retrofitService: CountriesApiService by lazy {
+        retrofit.create(CountriesApiService::class.java)
+    }
+}
